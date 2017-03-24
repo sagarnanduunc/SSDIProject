ec359e2091f1811c0577286ffe1f19ea1f0e3387
prayas rode <prode1@uncc.edu>
2017-03-23 21:55:03 -0400

    product dao

--- SSDIWebProject/src/main/java/com/ssdi/Dao/ProductDao.java
+++ SSDIWebProject/src/main/java/com/ssdi/Dao/ProductDao.java
@@ -25,17 +25,34 @@ public class ProductDao implements IProductDao {
     @Override
     public Collection<Product> getAllProducts() {
 
-        final String sql = "SELECT name, description, price FROM product";
+        final String sql = "SELECT product_id, name, description, price FROM product";
         List<Product> products = jdbcTemplate.query(sql, new RowMapper<Product>() {
             @Override
             public Product mapRow(ResultSet resultSet, int i) throws SQLException {
                 Product product = new Product();
+                product.setId(resultSet.getInt("product_id"));
                 product.setName(resultSet.getString("name"));
                 product.setDescription(resultSet.getString("description"));
                 product.setPrice(resultSet.getFloat("price"));
                 return product;
             }
         });
+        final String sql1 = "SELECT product_id, photo_link FROM photo";
+        List<Product> a=jdbcTemplate.query(sql1, new RowMapper<Product>() {
+            @Override
+            public Product mapRow(ResultSet resultSet, int i) throws SQLException {
+                int id=resultSet.getInt("product_id");
+                for(int j=0;j<products.size();j++)
+                {
+                    if(id == products.get(j).getId()){
+                        products.get(j).setPhotoLink(resultSet.getString("photo_link"));
+                    }
+                }
+                return null;
+            }
+        });
+
+
         return products;
     }
 
