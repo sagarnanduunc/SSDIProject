4c32756edfadadf4b43de384b9e681b285effb3f
Admin <snandu1@uncc.edu>
2017-03-23 20:00:23 -0400

    Adding files that somehow slipped out of a previous commit

--- SSDIWebProject/src/main/java/com/ssdi/Entity/Product.java
+++ SSDIWebProject/src/main/java/com/ssdi/Entity/Product.java
@@ -1,25 +1,34 @@
 package com.ssdi.Entity;
-
+import java.util.ArrayList;
 /**
  * Created by prayas on 3/19/2017.
  */
 public class Product implements IProduct{
+    private int id;
     private String name;
     private String description;
     private float price;
     private String category;
+    private ArrayList<String> photoLink=new ArrayList<String>();
 
-    public Product(String name, String description, float price, String category){
+    public Product(int id,String name, String description, float price, String category, ArrayList<String> photoLink){
+        this.id=id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.category = category;
+
+        for(int i=0;i<photoLink.size();i++)
+            this.photoLink.add(photoLink.get(i));
     }
 
     public Product(){
 
     }
-
+    public int getId() {
+        return id;
+    }
+    public void setId(int id){this.id=id;}
     public String getName() {
         return name;
     }
@@ -50,6 +59,14 @@ public class Product implements IProduct{
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public ArrayList<String> getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink.add(photoLink);
     }
 
 }
